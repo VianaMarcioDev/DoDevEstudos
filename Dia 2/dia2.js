e64163707119ba37bc4391006b37576eb8bdea64
VianaMarcioDev <marcioviana.developer@gmail.com>
2024-04-04 07:22:21 -0300

    Exercicio IMC

--- Dia 2/dia2.js
+++ Dia 2/dia2.js
@@ -1,1 +1,10 @@
-console.log("Dia 2")
+let nome = prompt("Digite seu nome: ")
+let idade = parseInt(prompt("Digite sua idade"))
+let altura = Number(prompt("Digite sua altura"))
+let peso = Number(prompt("Digite seu peso"))
+let anoAtual = parseInt(2023)
+let anoNascimento = anoAtual - idade
+let imc = peso / (altura * altura)
+
+console.log("Olá " + nome + ", você tem " + idade + " anos. nasceu em " + anoNascimento + ", tem " + altura +
+" de altura, pesa " + peso + "kg e seu IMC é " + imc + "kg/m2")
